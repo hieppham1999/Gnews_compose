d80c3343f63888734947ed5053eb142b0c233227
hieppham1999 <hieppham1999@gmail.com>
2023-10-01 22:07:56 +0700

    update api key

--- app/src/main/java/com/hieppt/enrich/gnew/values/constants.kt
+++ app/src/main/java/com/hieppt/enrich/gnew/values/constants.kt
@@ -1,5 +1,4 @@
 package com.hieppt.enrich.gnew.values
 
-//const val GNEWS_API_KEY = "412577edaca77aebb24573dbb5483306"
-const val GNEWS_API_KEY = "3fb460904666a32aae12292be7d0465c"
+const val GNEWS_API_KEY = "412577edaca77aebb24573dbb5483306"
 const val GNEWS_BASE_URL = "https://gnews.io/api/v4/"
